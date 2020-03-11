5b83cbbb863fce7b0d44f19571486888faab0d62
Pranav Karawale <pdkarawale31@gmail.com>
2020-03-11 18:09:57 +0530

    chore: fix typo

--- test/vdom-specialprops.test.ts
+++ test/vdom-specialprops.test.ts
@@ -10,14 +10,14 @@ describe("TeddyVDOM - special properties", () => {
   });
   it("should set 'innerHTML' of the component", () => {
     let app = h("h1", { innerHTML: "This is innerHTML" });
-    render(app, document.querySelector("test"));
+    render(app, document.querySelector("#test"));
     expect(document.querySelector("#test h1").innerHTML).toBe(
       "This is innerHTML"
     );
   });
   it("should set 'class' of the component through 'className'", () => {
     let app = h("h1", { className: "class" });
-    render(app, document.querySelector("test"));
+    render(app, document.querySelector("#test"));
     expect(document.querySelector("#test h1").className).toBe("class");
   });
 });
